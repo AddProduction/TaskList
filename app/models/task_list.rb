9ad48c0926614287debdc9205871e8ab973c72ca
ryuichi.igawa <addproduction007@gmail.com>
2018-11-05 13:46:21 +0000

    kadai-Bootstrap

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,3 +1,8 @@
 class TaskList < ApplicationRecord
     validates :status, presence: true, length: {maximum: 10}
+    
+    def select(keyword)
+        words = { done: 1, yet: 2 }
+        self.status = words[keyword.to_sym].to_i
+    end
 end
